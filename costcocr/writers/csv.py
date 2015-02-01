2bc249dc4996c0cccfe61a3d8bf1658fa987e7cf
Ryan Orendorff <ryan@rdodesigns.com>
2015-02-01 01:45:31 -0800

    Convert CSV writer to a module definition.
    Use __import__ to import it as a dictionary.

--- costcocr/writers/csv.py
+++ costcocr/writers/csv.py
@@ -1,33 +1,22 @@
+def Receipt(meta, body, variables):
+    output = []
 
-def csv():
+    def add(s) : output.append(s)
 
-    def Receipt(meta, body, variables):
-        output = []
+    if "store" in meta:
+        add("# Store: {}".format(meta["store"]))
+    if "date" in meta:
+        add("# Date: {}".format(meta["date"]))
+    if "location" in meta:
+        add("# Location: {}".format(meta["location"]))
 
-        def add(s) : output.append(s)
+    add(body)
 
-        if "store" in meta:
-            add("# Store: {}".format(meta["store"]))
-        if "date" in meta:
-            add("# Date: {}".format(meta["date"]))
-        if "location" in meta:
-            add("# Location: {}".format(meta["location"]))
+    return "\n".join(output)
 
-        add(body)
+def ItemList(s): return s
+def ItemListSep(): return "\n"
 
-        return "\n".join(output)
-
-    def ItemList(s): return s
-    def ItemListSep(): return "\n"
-
-    ## Could also be used to collapse the fields, or exclude some.
-    def Item(name, cost, discount, tax):
-        return "{}, {}, {}, {}".format(name, cost, discount, tax)
-
-    return {
-            "Receipt" : Receipt,
-            "ItemList" : ItemList,
-            "ItemListSep" : ItemListSep,
-            "Item" : Item,
-        }
-
+## Could also be used to collapse the fields, or exclude some.
+def Item(name, cost, discount, tax):
+    return "{}, {}, {}, {}".format(name, cost, discount, tax)
